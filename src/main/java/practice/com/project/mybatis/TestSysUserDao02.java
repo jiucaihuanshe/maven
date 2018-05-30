a54c37272c05476182dd9fa7e8be836d32027950
jiucaihuanshe <948710286@qq.com>
2018-05-31 00:34:13 +0800

    mybatis useGeneratedKeys=”true” order by ${columnName} @Param

--- src/main/java/practice/com/project/mybatis/TestSysUserDao02.java
+++ src/main/java/practice/com/project/mybatis/TestSysUserDao02.java
@@ -15,15 +15,22 @@ import org.junit.Test;
 
 import practice.com.project.mybatis.dao.SysUser;
 import practice.com.project.mybatis.dao.SysUserDao;
+import practice.com.project.mybatis.vo.Account;
+
 
 public class TestSysUserDao02 {
+	/**
+	 * 每个基于Mybatis的应用都是以一个SqlSessionFactory的实例为中心的。
+	 */
 	private SqlSessionFactory factory;
 	@Before
 	public void init() throws IOException{
+		//SqlSessionFactory的实例可以通过SqlSessionFactoryBuilder基于xml的配置创建。
 		factory=new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
 	}
 	@Test
 	public void testinsert(){
+		//SqlSession对象代表了一次SQL会话，包含了面向数据库执行SQL命令所需的所有方法。
 		SqlSession session = factory.openSession(false);
 		SysUserDao dao =session.getMapper(SysUserDao.class);
 		SysUser entity = new SysUser();
@@ -48,13 +55,58 @@ public class TestSysUserDao02 {
 		SqlSession session = factory.openSession(false);
 		SysUserDao dao = session.getMapper(SysUserDao.class);
 		SysUser entity = new SysUser();
-		entity.setUsername("xiao1");
+		entity.setUsername("xiao");
 		entity.setPassword("456");
 		entity.setPhone("13877");
-		entity.setId(5);
+		entity.setId(7);
 		int map = dao.updateObject(entity);
 		session.commit();
 		System.out.println(entity);
+		session.close();
+	}
+	@Test
+	public void testdelete(){
+		SqlSession session = factory.openSession(false);
+		SysUserDao dao = session.getMapper(SysUserDao.class);
+		SysUser entity = new SysUser();
+		entity.setId(8);
+		int map = dao.deleteObject(entity);
+		session.commit();
+		System.out.println(map);
+		session.close();
+	}
+	@Test
+	public void testresultmap(){
+		SqlSession session= factory.openSession();
+		SysUserDao dao = session.getMapper(SysUserDao.class);
+		//要想输入Acount中的值，需要重写toString方法
+		List<Account> list = dao.findNameAndPwd();
+		System.out.println(list);
+		session.close();
+	}
+	//测试自增长id
+	@Test
+	public void testinsertObject1(){
+		SqlSession session = factory.openSession(false);
+		SysUserDao dao = session.getMapper(SysUserDao.class);
+		SysUser entity = new SysUser();
+		entity.setUsername("jiejie");
+		entity.setPassword("888");
+		entity.setPhone("1348569");
+		
+		int rows = dao.insertObject1(entity);
+		session.commit();
+		System.out.println("rows="+rows);
+		System.out.println(entity);
+		session.close();
+	}
+	//测试${} 传入值
+	@Test
+	public void testfindUsers1(){
+		SqlSession session = factory.openSession();
+		SysUserDao dao = session.getMapper(SysUserDao.class);
+		List<Map<String, Object>> list=dao.findUsers1("id","139");
+		System.out.println(list);
 		session.close();
 	}
 }
--- src/main/java/practice/com/project/mybatis/dao/SysUserDao.java
+++ src/main/java/practice/com/project/mybatis/dao/SysUserDao.java
@@ -3,9 +3,23 @@ package practice.com.project.mybatis.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
+import practice.com.project.mybatis.vo.Account;
+
 public interface SysUserDao {
 	List<Map<String, Object>> findUsers();
 	Map<String, Object> findUserById(Integer id);
 	int insertObject(SysUser entity);
 	int updateObject(SysUser entity);
+	int deleteObject(SysUser entity);
+	
+	//测试resultMap
+	List<Account> findNameAndPwd();
+	//测试id自增长
+	int insertObject1(SysUser entity);
+	//测试会改变的字符串
+	//假如使用$取参数的值，此时要使用@Param注解对参数进行声明
+	//当使用#获取参数数值时，假如接口方法中有多个参数，使用@Param注解进行声明
+	List<Map<String, Object>> findUsers1(@Param("columnName") String columnName,@Param("phone") String phone);
 }
